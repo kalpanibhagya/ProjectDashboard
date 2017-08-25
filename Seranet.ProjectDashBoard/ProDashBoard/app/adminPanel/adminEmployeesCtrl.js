d561db36195adf085bfac8f8680473d42b6499ae
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-08-25 11:45:26 +0530

    Modify adminEmployeesCtrl.js Fix #4

--- Seranet.ProjectDashBoard/ProDashBoard/app/adminPanel/adminEmployeesCtrl.js
+++ Seranet.ProjectDashBoard/ProDashBoard/app/adminPanel/adminEmployeesCtrl.js
@@ -1,11 +1,11 @@
 ﻿(function () {
-    'use strict';
-    //create angularjs controller
+  'use strict';
+  //create angularjs controller
 
-    app.controller('adminEmployeesController', ['$scope', '$rootScope', '$http', '$window', adminEmployeesController]);
+  app.controller('adminEmployeesController', ['$scope', '$rootScope', '$http', '$window', adminEmployeesController]);
 
-    //angularjs controller method
-    function adminEmployeesController($scope, $rootScope, $http, $window) {
+  //angularjs controller method
+  function adminEmployeesController($scope, $rootScope, $http, $window) {
         
-    }
+  }
 })();
