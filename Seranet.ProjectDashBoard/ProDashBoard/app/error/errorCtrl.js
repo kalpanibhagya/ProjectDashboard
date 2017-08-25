677d23c19166506debde1fc5009c02ae02c2516e
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-08-26 05:13:43 +0530

    Modify errorCtrl.js Fix #4

--- Seranet.ProjectDashBoard/ProDashBoard/app/error/errorCtrl.js
+++ Seranet.ProjectDashBoard/ProDashBoard/app/error/errorCtrl.js
@@ -1,11 +1,11 @@
 ﻿(function () {
-    'use strict';
-    //create angularjs controller
+  'use strict';
+  //create angularjs controller
 
-    app.controller('errorController', ['$scope', '$http', '$stateParams', ErrorController]);
+  app.controller('errorController', ['$scope', '$http', '$stateParams', ErrorController]);
 
-    //angularjs controller method
-    function ErrorController($scope, $http, $stateParams) {
-        //alert("Unauthorized WarFare............Access denied");
-    }
+  //angularjs controller method
+  function ErrorController($scope, $http, $stateParams) {
+    //alert("Unauthorized WarFare............Access denied");
+  }
 })();
