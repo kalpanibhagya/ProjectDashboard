eec796fe43bfabfdb9a60abb69ea4539ed663244
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-08-31 20:48:37 +0530

    Fix indentation to 2 spaces Fix #4

--- Seranet.ProjectDashBoard/ProDashBoard/Models/CustomerSatisfactionQuestion.cs
+++ Seranet.ProjectDashBoard/ProDashBoard/Models/CustomerSatisfactionQuestion.cs
@@ -5,17 +5,17 @@ using System.Web;
 
 namespace ProDashBoard.Models
 {
-    public class CustomerSatisfactionQuestion
-    {
-        public int Id { get; set; }
-        public int AccountId { get; set; }
-        public int ProjectId { get; set; }
-        public int Year { get; set; }
-        public int Quarter { get; set; }
-        public int QuestionOrder { get; set; }
-        public string ActualQuestion { get; set; }
-        public string DashboardQuestion { get; set; }
-        public string Answer { get; set; }
-        public bool CalcExist { get; set; }
-    }
+  public class CustomerSatisfactionQuestion
+  {
+    public int Id { get; set; }
+    public int AccountId { get; set; }
+    public int ProjectId { get; set; }
+    public int Year { get; set; }
+    public int Quarter { get; set; }
+    public int QuestionOrder { get; set; }
+    public string ActualQuestion { get; set; }
+    public string DashboardQuestion { get; set; }
+    public string Answer { get; set; }
+    public bool CalcExist { get; set; }
+  }
 }
